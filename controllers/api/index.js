c414b4f2c5b90e57be7612f7bfc7bbfb42f6d225
Michael Leblanc <mleblanc1@werfen.com>
2023-10-05 21:09:20 -0400

    added code for routes for review route and user route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const bookRoutes = require('./bookRoutes');
 const reviewRoutes = require('./reviewRoutes');
 
 router.use('');
---
+++ controllers/api/reviewRoutes.js
@@ -0,0 +1,11 @@
+const router = ('express').Router();
+const { User } = require('../../models');
+
+router.post('/post-review', async (req, res) => {
+    try {
+        
+
+    } catch (err) {
+
+    }
+});

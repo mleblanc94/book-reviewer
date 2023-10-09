2a17f9cae7176c0d6964de9976aef725214d4413
Michael Leblanc <mleblanc1@werfen.com>
2023-10-09 17:34:14 -0400

    added code to the controller api folder that will allow a user to post a review

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -1,10 +1,18 @@
 const router = ('express').Router();
-const { User } = require('../../models');
+const { Review } = require('../../models');
 
 router.post('/post-review', async (req, res) => {
     try {
-        
-
+        const newReview = await Review.create({
+            user_id: req.session.user_id,
+            author_name: req.body.author_name,
+            book_name: req.body.bookName,
+            review: req.body.review,
+            book_cover: req.body.cover,
+            rating: req.body.rating,
+            description: req.body.description,
+        });
+        res.status(200).json(newReview);
     } catch (err) {
 
     }

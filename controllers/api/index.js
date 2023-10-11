ae8f693cc90fff129b13b7f3d6ab716f949afc74
Michael Leblanc <mleblanc1@werfen.com>
2023-10-11 19:27:38 -0400

    changed the route files to match what Sajith had done for the frontend to match up

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const reviewRoutes = require('./reviewRoutes');
 
-router.use('/user', userRoutes);
-
-router.use('/review', reviewRoutes);
+router.use('/users', userRoutes);
+router.use('/reviews', reviewRoutes);
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,30 +2,34 @@ const router = ('express').Router();
 const { User } = require('../../models');
 
 router.post('/signup', async (req,res) => {
-    try {
-        const userData = await User.create(req.body);
-
+    User.create({
+        name: req.body.name,
+        email: req.body.useremail,
+        password: req.body.password
+    })
+    .then(dbUserData => {
         req.session.save(() => {
-            req.session.user_id = userData.id;
+            req.session.user_id = dbUserData.id;
             req.session.logged_in = true;
-
-            req.status(200).json(userData);
+            res.json(dbUserData);
         });
-    } catch (err) {
-        res.status(400).json(err);
-    }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.post('/login', async (req,res) => {
     try {
-        const userData = await User.findOne({ where: { email: req.body.email } });
+        const userData = await User.findOne({ where: { email: req.body.strUserName } });
 
         if (!userData) {
             res.status(400).json({ message: 'Incorrect email or password, please try again' });
             return;
         }
 
-        const validPassword = await userData.checkPassword(req.body.password);
+        const validPassword = await userData.checkPassword(req.body.strPassWord);
 
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect email or password, please try again' });

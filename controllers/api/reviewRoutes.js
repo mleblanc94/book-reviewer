7bbc473cfba3884281890b7a0dda8c7a217e06c1
Michael Leblanc <mleblanc1@werfen.com>
2023-10-12 19:03:25 -0400

    fixed routes to align with Sajith to communicate with frontend

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -3,22 +3,27 @@ const { Review, Book, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // Post a new review
-router.post('/review', withAuth, async (req, res) => {
-    try {
-        const newReview = await Review.create({
-            user_id: req.session.user_id,
-            author_name: req.body.author_name,
-            book_name: req.body.bookName,
-            review: req.body.review,
-            book_cover: req.body.cover,
-            rating: req.body.rating,
-            description: req.body.description,
+router.post('/add', withAuth, async (req, res) => {
+    
+    Book.create({
+        book_name: req.body.book_name,
+        author_name: req.body.author_name,
+        book_cover: req.body.book_cover,
+        description: req.body.book_description
+    })
+        .then((createdBook) => {
+            return Review.create({
+                rating: req.body.rating,
+                comment: req.body.reviewContent,
+                user_id: req.session.user_id,
+                book_id: createdBook.id,
+            });
+        })
+        .then((createdReview) => res.json({ createdReview }))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
         });
-        res.status(200).json(newReview);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
 });
 
 // Get all reviews
@@ -32,8 +37,7 @@ router.get('/', async (req, res) => {
             model: Book,
             attributes: ["book_name", "author_name", "book_cover", "description"],
     },
-],
-    include: [{
+    {
         model: User,
         attributes: ["name"],
     },
@@ -52,28 +56,6 @@ router.get('/', async (req, res) => {
         console.log(err);
         res.status(500).json(err);
     })
-});
-
-// Delete a review
-router.delete("/:id", withAuth, (req, res) => {
-    Review.destroy({
-            where: {
-                id: req.params.id,
-            },
-        })
-        .then((dbPostData) => {
-            if (!dbPostData) {
-                res.status(404).json({
-                    message: "No review found with this id"
-                });
-                return;
-            }
-            res.json(dbPostData);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
 });
 
 router.get('/new', (req, res) => {

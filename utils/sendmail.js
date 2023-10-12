b07e9feba42e985ba00bc7653aa41dc051f76473
DMB-LEB <nhnative2020@gmail.com>
2023-10-11 20:33:00 -0400

    finished nodemailr transporter and put email PW in .env

--- utils/sendmail.js
+++ utils/sendmail.js
@@ -2,9 +2,12 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: true,
     auth: {
-      user: 'your_email@gmail.com',
-      pass: 'your_password'
+      user: 'bookreviewerproject@gmail.com',
+      pass: 'process.env.EMAIL_PASS'
     }
   });
 

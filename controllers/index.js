5ffaeda4647cab755157d5a0bb1e41394488ed1a
Michael Leblanc <mleblanc1@werfen.com>
2023-10-12 20:20:44 -0400

    fixed errors received while testing relating to db connection and nodemailer

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoute = require('./homeroute.js');
+const homeRoute = require('./homeRoute.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoute);

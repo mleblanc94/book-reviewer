b4cbc9bbd86b3ce4e2421286ef336d85feea1295
Michael Leblanc <mleblanc1@werfen.com>
2023-10-12 21:45:14 -0400

    heroku commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoute = require('./homeRoute.js');
+const homeRoute = require('./homeroute.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoute);
